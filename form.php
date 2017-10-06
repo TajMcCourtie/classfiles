3524c4d9fbb91858343fcc19644f8f55d489bea9
TajMcCourtie <31289979+TajMcCourtie@users.noreply.github.com>
2017-10-06 12:33:03 -0500

    Create form.php

---
+++ form.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head></head>
+    
+    <body>
+ <form action ="validate.php?page=information" method="post">
+    <input type="text" name="firstname"><br>
+     <input type="radio" name="gender" value="male">Male <br>
+     <input type="radio" name="gender" value="Female">Female <br>
+     <br>
+    <input type="submit" value="Submit" name="button"/>
+    <!-- <input type="submit" value="Cancel" name="button"/>-->
+</form>
+    </body>
+</html>
+
